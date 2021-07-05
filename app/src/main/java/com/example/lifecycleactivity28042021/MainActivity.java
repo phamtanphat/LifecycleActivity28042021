642824d503124ff9b84f6eda7ba61f5b936f6630
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-05 20:00:29 +0700

    update

--- app/src/main/java/com/example/lifecycleactivity28042021/MainActivity.java
+++ app/src/main/java/com/example/lifecycleactivity28042021/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("BBB","onCreate");
+        Log.d("BBB","MainActivity : onCreate");
 
         mBtnNavigateSecondActivity = findViewById(R.id.buttonNavigateSecondActivity);
 
@@ -31,36 +31,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("BBB","onStart");
+        Log.d("BBB","MainActivity : onStart");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("BBB","onPause");
+        Log.d("BBB","MainActivity : onPause");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("BBB","onResume");
+        Log.d("BBB","MainActivity : onResume");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("BBB","onStop");
+        Log.d("BBB","MainActivity : onStop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d("BBB","onRestart");
+        Log.d("BBB","MainActivity : onRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("BBB","onDestroy");
+        Log.d("BBB","MainActivity : onDestroy");
     }
 }
--- app/src/main/java/com/example/lifecycleactivity28042021/SecondActivity.java
+++ app/src/main/java/com/example/lifecycleactivity28042021/SecondActivity.java
@@ -3,12 +3,62 @@ package com.example.lifecycleactivity28042021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
+    Button mBtnNavigateThirdActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Log.d("BBB","SecondActivity : onCreate");
+
+        mBtnNavigateThirdActivity = findViewById(R.id.buttonNavigateThirdActivity);
+
+        mBtnNavigateThirdActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("BBB","SecondActivity : onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("BBB","SecondActivity : onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("BBB","SecondActivity : onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("BBB","SecondActivity : onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("BBB","SecondActivity : onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("BBB","SecondActivity : onDestroy");
     }
 }
